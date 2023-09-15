434c0062dae12831813752e3b22bbe0dd3cc17fe
XiaoY <exidot@outlook.com>
2023-09-15 08:44:42 +0800

    second

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,10 +1,10 @@
 <template>
-  <!-- <Logo></Logo> -->
+  <Logo></Logo>
   <Link></Link>
 </template>
 
 <script setup lang="ts">
-// import Logo from '@/components/logo.vue'
+import Logo from '@/components/logo.vue'
 import Link from '@/components/link.vue'
 
 /**
